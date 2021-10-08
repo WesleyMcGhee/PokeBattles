465f08b4b2474060bfaffa5947f04f38ad39000c
Wesley McGhee <wesmcghee6372@gmail.com>
2021-10-08 09:39:34 -0500

    preparing for deployment

--- server/index.js
+++ server/index.js
@@ -2,3 +2,12 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 4000;
+
+app.get(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public"));
+});
+app.use(express.json());
+app.use(cors());
+
+app.listen(port, () => console.log(`Server is up and running on ${port}...`));
